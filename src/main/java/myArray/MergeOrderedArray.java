42c54fc009f8fd8c3ecc3bfdbfcb555a172cfc61
45live <694375330@qq.com>
2024-05-20 14:00:00 +0800

    更新算法

--- src/main/java/myArray/MergeOrderedArray.java
+++ src/main/java/myArray/MergeOrderedArray.java
@@ -6,13 +6,13 @@ public class MergeOrderedArray {
     public static void main(String[] args) {
         int[] a1 = {1, 5, 6, 2, 4, 10, 11};
         int[] a2 = new int[a1.length];
-        new MergeOrderedArray().merge(a1, 0, 2, 3, 6, a2, 0);
+        new MergeOrderedArray().merge2(a1, 0, 2, 3, 6, a2);
         System.out.println(Arrays.toString(a2));
         //如果想要将a1的数据更新为合并后的结果，只需要将a2中的元素拷贝给a1就可以了
     }
 
     /**
-     * LeetCode88：将两个有序区间的的有序元素合并，合并后，结果仍然存储在原有空间
+     * LeetCode88：使用递归思路进行实现：将两个有序区间的的有序元素合并，合并后，结果仍然存储在原有空间
      *
      * @param nums1 合并的之前原始数组
      * @param i     第一个有序区间的起点
@@ -22,7 +22,7 @@ public class MergeOrderedArray {
      * @param nums2 合并之后的结果数组
      * @param k     num2[]数组使用的索引
      */
-    public void merge(int[] nums1, int i, int iEnd, int j, int jEnd, int[] nums2, int k) {
+    public void merge1(int[] nums1, int i, int iEnd, int j, int jEnd, int[] nums2, int k) {
         /*
         将两个有序区间中的元素进行比较，谁更小，就将谁放到结果数组中
         每次将更小的元素放在结果数组中即可
@@ -37,10 +37,32 @@ public class MergeOrderedArray {
         }
         if (nums1[i] < nums1[j]) {
             nums2[k] = nums1[i];
-            merge(nums1, i + 1, iEnd, j, jEnd, nums2, k + 1);
+            merge1(nums1, i + 1, iEnd, j, jEnd, nums2, k + 1);
         } else {
             nums2[k] = nums1[j];
-            merge(nums1, i, iEnd, j + 1, jEnd, nums2, k + 1);
+            merge1(nums1, i, iEnd, j + 1, jEnd, nums2, k + 1);
+        }
+    }
+
+    public void merge2(int[] nums1, int i, int iEnd, int j, int jEnd, int[] nums2) {
+        //使用非递归思路实现，循环判断：
+        //将索引k改成方法内的局部变量
+        int k = 0;
+        while (i <= iEnd && j <= jEnd) {
+            if (nums1[i] < nums1[j]) {
+                nums2[k] = nums1[i];
+                i++;
+            } else {
+                nums2[k] = nums1[j];
+                j++;
+            }
+            k++;
+        }
+        if (i > iEnd) {
+            System.arraycopy(nums1, j, nums2, k, jEnd - j + 1);
+        }
+        if (j > jEnd) {
+            System.arraycopy(nums1, i, nums2, k, iEnd - i + 1);
         }
     }
 }
---
+++ src/main/java/myQueue/LinkedListImitateQueue.java
@@ -0,0 +1,7 @@
+package myQueue;
+
+public class LinkedListImitateQueue {
+    public static void main(String[] args) {
+        
+    }
+}
