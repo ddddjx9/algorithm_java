0c0c18560ae052ce2e8bac51bd96a353c022ec08
45live <694375330@qq.com>
2024-06-23 09:39:56 +0800

    update

--- src/main/java/graph/TopologicalSortDFS.java
+++ src/main/java/graph/TopologicalSortDFS.java
@@ -20,7 +20,7 @@ public class TopologicalSortDFS {
         v6.edges = List.of(new Edge(v4));
         v4.edges = List.of(new Edge(v5));
         v5.edges = List.of(new Edge(v7));
-        v7.edges = List.of();
+        v7.edges = List.of(new Edge(v5));
 
         List<Vertex> graph = new ArrayList<>(List.of(v1, v2, v3, v4, v5, v6, v7));
         dfsTopologicalSort(graph);
@@ -31,14 +31,29 @@ public class TopologicalSortDFS {
         for (Vertex vertex : graph) {
             dfs(vertex, stack);
         }
+        System.out.println(stack);
     }
 
     private static void dfs(Vertex v, LinkedList<String> stack) {
         if (v.status == 2) {
             return;
         }
+
+        //下面的代码都是用于检测环的出现，如果不需要，直接将有代码的这三行删除即可
+        //每次进入递归调用的时候加入判断，判断当前状态是否为正在访问中的状态
+        //如果是正在访问中的状态，说明出现了环
+        if (v.status == 1) {
+            throw new RuntimeException("检测出环！");
+        }
+        //将status设置为1
+        v.status = 1; //用于检测环的出现
+
+        //处理相邻顶点
         for (Edge edge : v.edges) {
             dfs(edge.linked, stack);
         }
+        //处理完毕，回溯的时候更新状态并压栈
+        v.status = 2;
+        stack.push(v.name);
     }
 }
--- src/main/java/graph/Vertex.java
+++ src/main/java/graph/Vertex.java
@@ -15,7 +15,7 @@ public class Vertex {
     List<Edge> edges; //顶点关联的边的集合
     boolean visited; //代表该顶点在遍历过程中是否被访问过
     int inDegree; //顶点的入度
-    int status; //状态 0-未访问 1-访问中 2-访问过
+    int status; //状态 0-未访问 1-访问中 2-访问过  1的状态主要是用于检测拓扑排序中环的出现
 
     public Vertex(String name) {
         this.name = name;
