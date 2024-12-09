b9f79d80a3a388dbda083e262ab8d1977e8d9b46
i-dongjiaxin <i-dongjiaxin@stepfun.com>
2024-12-09 16:42:40 +0800

    update

---
+++ src/main/java/leetcode/array/LeetCode53.java
@@ -0,0 +1,25 @@
+package leetcode.array;
+
+import java.util.HashMap;
+
+public class LeetCode53 {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new LeetCode53().maxSubArray(nums));
+    }
+
+    /**
+     * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * <p>
+     * 子数组是数组中的一个连续部分。
+     * </p>
+     *
+     * @param nums 给定的整数数组
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        // 暴力解法，直接暴力累加
+        HashMap<String, String> map = new HashMap<>();
+        return 0;
+    }
+}
