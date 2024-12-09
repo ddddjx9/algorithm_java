35521fb961a74bca605384235c25f464c2688239
i-dongjiaxin <i-dongjiaxin@stepfun.com>
2024-12-09 17:15:59 +0800

    update

--- src/main/java/leetcode/array/LeetCode53.java
+++ src/main/java/leetcode/array/LeetCode53.java
@@ -6,6 +6,7 @@ public class LeetCode53 {
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(new LeetCode53().maxSubArray(nums));
+        System.out.println(new LeetCode53().maxSubArray2(nums));
     }
 
     /**
@@ -18,13 +19,33 @@ public class LeetCode53 {
      * @return 返回一个具有最大和的连续子数组
      */
     public int maxSubArray(int[] nums) {
-        int sum = 0;
-        Arrays.sort(nums);
-        for (int num : nums) {
-            if (num > 0) {
-                sum += num;
-            }
+        // 难点是如何在数组分布未知的情况下，判断累加的分界点在哪里，现在的解决方案是暴力累加
+        // 典型的动态规划问题，主要的点是如何确定状态转移方程
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
         }
-        return sum;
+
+        Arrays.sort(dp);
+        return dp[nums.length - 1];
+    }
+
+    public int maxSubArray2(int[] nums) {
+        // 处理空数组情况
+        if (nums.length == 0) return 0;
+
+        // 当前最大子数组和
+        int maxSoFar = nums[0];
+        // 当前子数组和
+        int currentMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+
+        // 返回最大子数组和
+        return maxSoFar;
     }
 }
